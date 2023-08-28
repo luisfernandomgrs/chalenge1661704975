b5309ab19cf9e086577fbb4e9dccdf50ef3d3d55
Luis Fernando Machado <>
2023-08-28 01:06:17 -0300

    first commit

---
+++ backend/src/test/java/com/studentluisf/personcatalog/ClientCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentluisf.personcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
