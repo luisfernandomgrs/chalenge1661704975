333fd714868eb3460e78f855e8ddee228d58ae7d
Luis Fernando Machado <>
2023-08-28 01:15:24 -0300

    Resource layer and Exception class, Client

---
+++ backend/src/main/java/com/studentluisf/personcatalog/resources/ClientResource.java
@@ -0,0 +1,31 @@
+package com.studentluisf.personcatalog.resources;
+
+import com.studentluisf.personcatalog.dto.ClientDTO;
+import com.studentluisf.personcatalog.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                                   @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+                                                   @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+                                                   @RequestParam(value = "orderBy", defaultValue = "name") String orderBy) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return ResponseEntity.ok(this.clientService.findAllPaged(pageRequest));
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(clientService.findById(id));
+    }
+}
---
+++ backend/src/main/java/com/studentluisf/personcatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.studentluisf.personcatalog.resources.exceptions;
+
+import com.studentluisf.personcatalog.services.exceptions.DatabaseException;
+import com.studentluisf.personcatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource not found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError error = new StandardError();
+
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Database exception");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+
+        return ResponseEntity.status(status).body(error);
+    }
+}
