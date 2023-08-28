a2ae9a684e8bdcb77c545385e155dbd73a380a3d
Luis Fernando Machado <>
2023-08-28 01:08:50 -0300

    project settings

---
+++ backend/src/main/java/com/studentluisf/personcatalog/ClientCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.studentluisf.personcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCatalogApplication.class, args);
+	}
+
+}
